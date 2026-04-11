54a6240c6103401a4cd68a3715c003926f957097
Manuelcortes07 <mcorcon0109@g.educaand.es>
2026-04-11 23:25:49 +0200

    chore: preparar despliegue Railway

--- app.py
+++ app.py
@@ -3496,5 +3496,6 @@ def internal_error(error):
 # crear_admin_inicial()
 
 #  EJECUCIÓN
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
